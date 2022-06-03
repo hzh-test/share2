29ef759977465782d8e5e1dec0ff92ed28d39a19
hzh-test <1782430063@qq.com>
2022-06-03 11:11:43 +0800

    v0.0.0

---
+++ go.mod
@@ -0,0 +1,5 @@
+module share2
+
+go 1.18
+
+require github.com/hzh-test/share v1.1.2 // indirect
---
+++ share2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+import "github.com/hzh-test/share"
+
+func Sh2() {
+	fmt.Println(share.Ss)
+	share.Sh()
+
+    fmt.Println("share2 v0.0.0")
+    fmt.Println("111")
+}
+
